6e3d8dfd4eb6e5015069d74423e0b71b2e901ea0
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-03-18 23:41:16 +0000

    Refactored code for Projects

--- my-portfolio/src/components/Project.tsx
+++ my-portfolio/src/components/Project.tsx
@@ -7,12 +7,39 @@ import Vocabio from "../assets/vocabio.png";
 
 function Project() {
   const gitHubLink = "https://github.com/A-Moiz";
-  const ListItLink =
-    "https://apps.apple.com/gb/app/list-it-simple-to-do-list/id6746731233";
-  const bmiBuddyLink = "https://apps.apple.com/gb/app/bmi-buddy/id6683310423";
-  const opbrCompanionLink =
-    "https://apps.apple.com/gb/app/opbr-companion/id6737994116";
-  const voabioLink = "https://apps.apple.com/gb/app/vocabio/id6757109154";
+  const projectsData = [
+    {
+      title: "List It",
+      description:
+        "A minimalist To-Do list and Note-taking app designed for speed and simplicity on iOS.",
+      image: ListIt,
+      href: "https://apps.apple.com/gb/app/list-it-simple-to-do-list/id6746731233",
+    },
+
+    {
+      title: "OPBR Companion",
+      description:
+        "A comprehensive guide and companion utility for One Piece Bounty Rush players to optimise their gameplay.",
+      image: OpbrCompanion,
+      href: "https://apps.apple.com/gb/app/opbr-companion/id6737994116",
+    },
+
+    {
+      title: "BMI Buddy",
+      description:
+        "A streamlined health utility providing a quick and easy way for users to calculate and track their BMI.",
+      image: BmiBuddy,
+      href: "https://apps.apple.com/gb/app/bmi-buddy/id6683310423",
+    },
+
+    {
+      title: "Vocabio",
+      description:
+        "A community driven slang dictionary where users contribute definitions and vote on the best entries in a live feed.",
+      image: Vocabio,
+      href: "https://apps.apple.com/gb/app/vocabio/id6757109154",
+    },
+  ];
 
   return (
     <section
@@ -43,33 +70,9 @@ function Project() {
 
         {/* Projects Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <ProjectCard
-            title="List It"
-            description="A minimalist To-Do list and Note-taking app designed for speed and simplicity on iOS."
-            image={ListIt}
-            href={ListItLink}
-          />
-
-          <ProjectCard
-            title="OPBR Companion"
-            description="A comprehensive guide and companion utility for One Piece Bounty Rush players to optimise their gameplay."
-            image={OpbrCompanion}
-            href={opbrCompanionLink}
-          />
-
-          <ProjectCard
-            title="BMI Buddy"
-            description="A streamlined health utility providing a quick and easy way for users to calculate and track their BMI."
-            image={BmiBuddy}
-            href={bmiBuddyLink}
-          />
-
-          <ProjectCard
-            title="Vocabio"
-            description="A community-driven slang dictionary where users contribute definitions and vote on the best entries in a live feed."
-            image={Vocabio}
-            href={voabioLink}
-          />
+          {projectsData.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
         </div>
       </div>
     </section>
